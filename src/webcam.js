b745e48b1690864489c0a677d0f5127a9102ecfc
Parvez Kose <flamboyanz@gmail.com>
2019-06-05 20:35:55 -0400

    refactor webcam

--- src/webcam.js
+++ src/webcam.js
@@ -1,40 +1,42 @@
 class WebCam {
+  constructor() {}
 
-    constructor(){
+  async setupWebcam(webcamElement) {
+    var constraints = { audio: true, video: true };
+    let mediaStream = null;
+    try {
+      mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+      /* use the stream */
+    } catch (err) {
+      /* handle the error */
     }
 
-    async setupWebcam(webcamElement) {
+    return new Promise((resolve, reject) => {
+      const navigatorAny = navigator;
+      navigator.getUserMedia =
+        navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia ||
+        navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
 
-      var constraints = { audio: true, video: true }
-      let mediaStream = null;
-      try {
-        mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
-        /* use the stream */
-      } catch(err) {
-        /* handle the error */
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia(
+          { video: true },
+          stream => {
+            webcamElement.srcObject = stream;
+            webcamElement.addEventListener(
+              "loadeddata",
+              () => resolve(),
+              false
+            );
+          },
+          error => reject()
+        );
+      } else {
+        reject();
       }
-
-      return new Promise((resolve, reject) => {
-        const navigatorAny = navigator;
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-            navigatorAny.msGetUserMedia;
-        
-        if (navigator.getUserMedia) {
-          navigator.getUserMedia({video: true},
-            stream => {
-              webcamElement.srcObject = stream;
-              webcamElement.addEventListener('loadeddata',  () => resolve(), false);
-            },
-            error => reject());
-            
-        } else {
-          reject();
-        }
-        
-      });
-      }
+    });
+  }
 }
 
 export default WebCam;
-
